831a4207bd2b86b8b77f65fb257b51115717138b
David van Laatum <david@vanlaatum.id.au>
2025-03-01 16:22:09 +1030

    initial

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -7,41 +7,49 @@ import ErrorPage from '@src/ErrorPage.tsx'
 export function Login() {
   const api = useApi()
   const [providers, setProviders] = useState<UserProvider[] | null>(null)
+  const [loading, setLoading] = useState(true)
   const [error, setError] = useState<unknown>(null)
 
   useEffect(() => {
-    const fetchProviders = async () => {
-      const providers = await api.userApi.getUserProviders()
-      setProviders(providers)
-      if (providers.length === 1) {
-        window.location.href = providers[0].loginURL
-      }
-    }
-    fetchProviders().catch((err: unknown) => {
-      setError(err)
-    })
+    api.userApi
+      .getUserProviders()
+      .then((providers) => {
+        setProviders(providers)
+        if (providers.length === 1) {
+          window.location.href = providers[0].loginURL
+        } else if (providers.length === 0) {
+          setError(new Error('No login providers available'))
+        }
+        setLoading(false)
+      })
+      .catch((err: unknown) => {
+        setError(err)
+      })
   }, [api.userApi])
 
   if (error) {
     return <ErrorPage error={error} />
   }
 
+  const providerList = () => {
+    if (loading) {
+      return <Spinner />
+    }
+    return providers?.map((provider) => (
+      <Button variant="primary" size="lg" key={provider.provider} as={'a'} href={provider.loginURL}>
+        {provider.loginButtonImageURL ? (
+          <img src={provider.loginButtonImageURL} alt={provider.provider} />
+        ) : (
+          'Login with ' + provider.displayName
+        )}
+      </Button>
+    ))
+  }
+
   return (
     <Container>
       <h1>Login</h1>
-      <div className="d-grid gap-2">
-        {(!providers || providers.length == 1) && <Spinner />}
-        {(providers?.length ?? 0) > 1 &&
-          providers?.map((provider) => (
-            <Button variant="primary" size="lg" key={provider.provider} as={'a'} href={provider.loginURL}>
-              {provider.loginButtonImageURL ? (
-                <img src={provider.loginButtonImageURL} alt={provider.provider} />
-              ) : (
-                'Login with ' + provider.displayName
-              )}
-            </Button>
-          ))}
-      </div>
+      <div className="d-grid gap-2">{providerList()}</div>
     </Container>
   )
 }
